4afd0291ce6deba2c0be8de4761c2337ebbe812f
Eef vanDongen <eef.vandongen@smhi.se>
2023-07-27 14:11:33 +0200

    load sitepackages in venv required to be able to use etk

--- eclair.py
+++ eclair.py
@@ -19,10 +19,23 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt5.QtWidgets import QAction, QMessageBox
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLabel
-import os
+
+
+
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLabel, QMessageBox
 from PyQt5.QtWidgets import QFileDialog
+
+# from .etk.src.etk.edb.importers import import_pointsources
+
+# import django
+
+import os
+import sys
+import subprocess
+import site
+
+
 
 
 class Eclair:
@@ -43,7 +56,9 @@ class Eclair:
         QMessageBox.information(None, 'Eclair', 'Click OK to get started')
         dialog = EclairDialog()
         dialog.exec_()
-        
+
+
+       
 class EclairDialog(QDialog):
     def __init__(self):
         super().__init__()
@@ -59,27 +74,54 @@ class EclairDialog(QDialog):
         layout.addWidget(label)
 
         # Add the QPushButton to the layout
-        btn_action = QPushButton("Choose file")
-        layout.addWidget(btn_action)
+        btn_action_import_pointsource = QPushButton("Dummy: Import pointsource")
+        layout.addWidget(btn_action_import_pointsource)
 
         # Connect the button click event to a function in your plugin's code
-        btn_action.clicked.connect(self.open_file_dialog)
+        btn_action_import_pointsource.clicked.connect(self.import_pointsource_dialog)
 
-    def open_file_dialog(self):
+        # Add the QPushButton to the layout
+        btn_action_import_django = QPushButton("Under construction: actual Import pointsource")
+        layout.addWidget(btn_action_import_django)
+        btn_action_import_django.clicked.connect(self.import_django_dialog)
+
+
+
+    def import_pointsource_dialog(self):
         # This function will be called when the button is clicked.
         # You can define the desired action here.
-        file_path, _ = QFileDialog.getOpenFileName(None, "Open External File", "", "Text Files (*.txt);;All Files (*)")
+        file_path, _ = QFileDialog.getOpenFileName(None, "Open pointsource file", "", "Spreadsheet file (*.xlsx) or comma-separated (*.csv)")
         if file_path:
-            with open(file_path, 'r') as file:
-                file_contents = file.read()
-                display_file_contents(file_contents)
+            ps = str(file_path) #import_pointsources(file_path)
+            display_ps_import_progress(ps)
+    
+    
+    def import_django_dialog(self):
+        # This function will be called when the button is clicked.
+        # Determine the path to the virtual environment
+        venv_path = os.path.join(os.path.dirname(__file__), '.venv')
+        site.addsitedir(os.path.join(venv_path, "lib", "python3.9", "site-packages"))
 
-def display_file_contents(contents):
+        import django
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etk.settings")
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
+        from etk.edb.importers import import_pointsources
+
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("Test, not actually importing django yet")
+        msg_box.setText("test")
+        msg_box.exec_()
+
+def display_ps_import_progress(ps_progress):
     # For this example, let's display the file contents in a message box.
     msg_box = QMessageBox()
-    msg_box.setWindowTitle("File Contents")
-    msg_box.setText(contents)
+    msg_box.setWindowTitle("Pointsource import progress")
+    msg_box.setText(ps_progress)
     msg_box.exec_()
+
+
+
 
 
 
